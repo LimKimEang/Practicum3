dee0edbcb94c30a351d4a9414b9775addafe0934
LimKimEang <44645273+LimKimEang@users.noreply.github.com>
2019-05-30 16:37:44 +0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,49 +1,30 @@
-#include <iostream>
-#include <math.h>
+#include<iostream>
 
 using namespace std;
 
 int main()
 {
-	int digits;
-	int first, second, third, forth, fiffth, six;
-	int third_left, third_right;
-
-	cout << "Enter 6digits:";
-	cin >> digits;
-	int n = digits;
+	int digits,n;
 	int count = 0;
-	while (n > 0) {
+
+	here:
+	cout << "Enter a four digit number: ";
+	cin >> digits;
+
+	//this is count digit statement
+	n = digits;
+	while ( n > 0) {
 		n = n / 10;
 		count++;
-	};
-	if (count ==6) {
-		first = digits / 100000;
-		second = digits / 10000 % 10;
-		third = digits / 1000 % 10;
-		forth = digits / 100 % 10;
-		fiffth = digits / 10 % 10;
-		six = digits % 10;
-		third_left = first + second + third;
-		third_right = forth + fiffth + six;
-		if (third_left == third_right)
-		cout << "This is a lucky number";
-			else
-		cout << "This is a not a lucky number";
-		}
-		else
-		{
-			cout << "Please input only 6 digits";
-		}
-		
-		cin.get();
-		cin.get();
-		return 0;
 	}
 
-	
-	
+	 if (count >5)
+		cout << "Completed !";
+	else
+		goto here; 
 
+	cin.get();
+	cin.get();
 
-
-	
+	return 0;
+}
